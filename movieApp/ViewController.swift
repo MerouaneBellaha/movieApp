05d3ce46e687f97f081bd8be88102ee0a3dc6fc6
Merouane Bellaha <m.bellaha@gmail.com>
2020-05-27 10:50:12 +0200

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Merouane Bellaha on 27/05/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
